5aba5e9b4fea0a0eef75875642c3830ad0c490f5
Julio <juliornellas@gmail.com>
2023-07-25 16:24:49 -0300

    config: TailwindCSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  variants: {
+    extend: {},
+  },
+};
