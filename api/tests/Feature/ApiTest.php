29ff76464f8cbdef7dd2b0ae49a307bd7399936d
Julio <juliornellas@gmail.com>
2023-07-26 00:31:52 -0300

    feat: New Test

--- api/tests/Feature/ApiTest.php
+++ api/tests/Feature/ApiTest.php
@@ -10,21 +10,4 @@ class ApiTest extends TestCase
 {
     use RefreshDatabase;
 
-    // public function test_weather_endpoint_returns_a_successful_response()
-    // {
-    //     $response = $this->get('/weather?lat=40.7128&lon=74.0060');
-
-    //     $response->assertStatus(200);
-    // }
-
-    // test getting the weather for a user
-    // public function test_user_weather_endpoint_returns_a_successful_response()
-    // {
-    //     $user = User::factory()->create();
-
-    //     $response = $this->get("/weather/{$user->id}");
-
-    //     $response->assertStatus(200);
-    // }
-
 }
